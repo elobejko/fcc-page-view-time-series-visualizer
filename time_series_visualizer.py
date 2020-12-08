601dff11e3fe90feb28aa936bdc90408c2a760c5
Ewa Łobejko <lobejkoewa@gmail.com>
2020-12-08 14:23:38 +0000

    Bar chart data prep

--- time_series_visualizer.py
+++ time_series_visualizer.py
@@ -14,7 +14,7 @@ df = df.loc[(df['value'] >= df['value'].quantile(0.025)) & (df['value'] <= df['v
 def draw_line_plot():
     # Draw line plot
 
-  fig, ax = plt.subplots(figsize = (12, 5)) 
+  fig, ax = plt.subplots(figsize = (12, 3)) 
   plt.plot(df, color = 'red')
   plt.xlabel('Date')
   plt.ylabel('Page Views')
@@ -27,19 +27,19 @@ def draw_line_plot():
 def draw_bar_plot():
     # Copy and modify data for monthly bar plot
     df_bar = df.copy()
+    df_bar.reset_index(inplace = True)
+    df_bar['year'] = [d.year for d in df_bar.date]
+    df_bar['month'] = [d.strftime('%b') for d in df_bar.date]
+    
+    tmp = pd.DataFrame()
     print(df_bar.head())
-    #df_bar['year'] = [d.year for d in df_bar['date']]
-    #df_bar['month'] = [d.strftime('%b') for d in df_bar.date]
-    #print(df_bar.head())
+
     # Draw bar plot
-
-
-
-
-
+    #fig, ax = df_bar.plot.bar(x = 'year', y = 'value')
+    
     # Save image and return fig (don't change this part)
    # fig.savefig('bar_plot.png')
-    #return fig
+   # return fig
 
 def draw_box_plot():
     # Prepare data for box plots (this part is done!)
